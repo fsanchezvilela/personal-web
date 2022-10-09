0119e7d036676d784dbc510ba6370f0cca37fdee
Fernando Sanchez Vilela <f.sanchezvilela@gmail.com>
2022-10-10 01:33:25 +0200

    feat: add wpo / seo / security headers / pwa (#3)
    * feat(pwa):add pwa feature
    
    * feat(pwa-security): add security headers layer and implement pwa HOC
    
    * feat(wpo):add security layers and create build
    
    * feat(site.webmanifest): add protocol_handlers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,90 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const withPWA =  require('next-pwa')
+const runtimeCaching = require('next-pwa/cache');
+
+const plugins = []
+// Content Security Policy
+const ContentSecurityPolicy = `
+   default-src 'self';
+   script-src 'self';
+   connect-src 'self' vitals.vercel-insights.com;
+   style-src 'self';
+   font-src 'self';  
+ `;
+
+/**
+ * Security:
+ *  X Content Type Options
+ *  X Frame Options
+ *  X XSS Protection
+ *  Security Policy
+ */
+const securityHeaders = () => [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
+
+plugins.push(withPWA)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  /*
+    Problem with 12.3.1 false error for property see more https://github.com/vercel/next.js/issues/39161
+  */
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  // Security Headers
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders(),
+      },
+    ];
+  },
+  // Webpack
+  webpack: (config) => {
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/,
+        include: /src/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      }),
+    )
+    return config
+  },
+};
 
-module.exports = nextConfig
+
+
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig)
