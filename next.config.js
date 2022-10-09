0f70f73f1c06ba9e053207076ca76f1833112d7c
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-10 00:43:02 +0200

    feat(pwa):add pwa feature

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  pwa: {
+    disable: process.env.NODE_ENV !== 'production',
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <meta
+        name="description"
+        content="Personal website of Fernando Sanchez Vilela (@fsanchezvilela)"
+      />
+      <meta name="theme-color" content="#333333" />
+      <meta name="referrer" content={'strict-origin'} />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      <link
+        href="/favicon-16x16.png"
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link
+        href="/favicon-32x32.png"
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+      />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
