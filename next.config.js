05d5caf2f0bf650b38b5a0cd94f540d75b74aa8f
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-10 00:47:55 +0200

    feat(pwa-security): add security headers layer and implement pwa HOC

--- next.config.js
+++ next.config.js
@@ -2,7 +2,41 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
-const nextConfig = {
+// Content Security Policy
+const ContentSecurityPolicy = `
+   default-src 'self';
+   script-src 'self';
+   connect-src 'self' vitals.vercel-insights.com;
+   style-src 'self';
+   font-src 'self';  
+ `;
+
+/**
+ * Security:
+ *  X Content Type Options
+ *  X Frame Options
+ *  X XSS Protection
+ *  Security Policy
+ */
+const securityHeaders = () => [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+const nextConfig = withPWA({
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
@@ -16,6 +50,15 @@ const nextConfig = {
     runtimeCaching,
     buildExcludes: [/middleware-manifest.json$/],
   },
-};
+  // Security Layer
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders(),
+      },
+    ];
+  },
+});
 
 module.exports = nextConfig;
