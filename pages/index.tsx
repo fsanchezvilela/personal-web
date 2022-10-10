38e9b05416fc4709fe15137683cf2219299794a8
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-10 02:02:45 +0200

    fix(homepage): add the right assets for vercel svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,12 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/assets/svg/vercel.svg"
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
