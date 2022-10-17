d3a4f2545516faf771fb1bca198ee0785d95fd51
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-17 07:41:17 +0200

    feat: add-header-basic-layout

--- src/common/view/layout/Header/index.tsx
+++ src/common/view/layout/Header/index.tsx
@@ -1,8 +1,22 @@
 // import { HeaderPropsType } from "./types";
 
+import Link from "next/link";
+import Image from "next/image";
+import { LinkAnchorNavigation } from "./constants";
+import BrandSVG from 'public/assets/svg/cell-molecule.svg'
+import { Button } from "../../components/atoms";
+
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="flex justify-between align-middle py-4">
+        <Image src={BrandSVG} className='bg-keppel-tint rounded-full border-charcoal filter transition ease-in-out hover:rotate-180 transform-gpu hover:sepia' objectFit='scale-down' width="64" height="64"  alt='brand-logo-svg'/>
+   
+
+      <section>
+        {LinkAnchorNavigation.map(({ hash, id, label }) => <Link key={id} href={hash}><Button buttonStyle="tertiary">{label}</Button></Link>)}
+      </section>
+
+    </header>
   )
 }
 
