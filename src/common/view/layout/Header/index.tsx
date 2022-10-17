84562d3fcbf54a5411152720f43545ade0095f4f
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-17 21:32:25 +0200

    feat: add correct width and height in brand logo

--- src/common/view/layout/Header/index.tsx
+++ src/common/view/layout/Header/index.tsx
@@ -8,7 +8,7 @@ import { Button } from "../../components/atoms";
 const Header = () => {
   return (
     <header className="flex justify-between align-middle py-4">
-        <Image src={BrandSVG} className='bg-keppel-tint rounded-full border-charcoal filter transition ease-in-out hover:rotate-180 transform-gpu hover:sepia' objectFit='scale-down' width="64" height="64"  alt='brand-logo-svg'/>
+      <Image src={BrandSVG} className='bg-keppel-tint rounded-full border-charcoal filter transition ease-in-out hover:rotate-180 transform-gpu hover:sepia' objectFit='scale-down' width={64} height={64}  alt='brand-logo-svg'/>
       <section>
         {LinkAnchorNavigation.map(({ hash, id, label }) => <Link key={id} href={hash}><Button buttonStyle="tertiary">{label}</Button></Link>)}
       </section>
